2f309d9d6a6f9c6e04391b507c16551cbac17f0b
JSK practicas <jctejadasaracho@gmail.com>
2013-07-25 09:25:55 +0200

    solved edit_user password sha1

--- edit_user.php
+++ edit_user.php
@@ -4,7 +4,7 @@ include_once("model/user_obj.php");
 $user_edit = new User;
 $user_edit->set_id($_POST['user_id']);
 $user_edit->set_username($_POST['username']);
-$user_edit->_save($_POST['password']);
+$user_edit->_save(sha1($_POST['password']));
 
 header('Location: http://localhost/dummy/index.php?page=admin&flang=users');
 ?>
